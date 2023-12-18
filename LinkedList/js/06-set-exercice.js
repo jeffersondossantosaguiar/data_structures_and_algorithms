c58bf7209f79a869641ee97f536241ee6fa3b2bc
Jefferson Aguiar <jefferson.aguiar@facens.br>
2023-12-18 11:48:18 -0300

    feat: ll set

--- LinkedList/js/06-set-exercice.js
+++ LinkedList/js/06-set-exercice.js
@@ -13,9 +13,42 @@ class LinkedList {
     this.length = 1
   }
 
+  printList() {
+    let temp = this.head
+    while (temp !== null) {
+      console.log(temp.value)
+      temp = temp.next
+    }
+  }
+
+  getHead() {
+    if (this.head === null) {
+      console.log("Head: null")
+    } else {
+      console.log("Head: " + this.head.value)
+    }
+  }
+
+  getTail() {
+    if (this.tail === null) {
+      console.log("Tail: null")
+    } else {
+      console.log("Tail: " + this.tail.value)
+    }
+  }
+
+  getLength() {
+    console.log("Length: " + this.length)
+  }
+
+  makeEmpty() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
   push(value) {
     const newNode = new Node(value)
-
     if (!this.head) {
       this.head = newNode
       this.tail = newNode
@@ -23,13 +56,12 @@ class LinkedList {
       this.tail.next = newNode
       this.tail = newNode
     }
-
     this.length++
     return this
   }
 
   pop() {
-    if (!this.head) return undefined
+    if (this.length === 0) return undefined
     let temp = this.head
     let pre = this.head
     while (temp.next) {
@@ -60,17 +92,19 @@ class LinkedList {
   }
 
   shift() {
-    if (!this.head) return undefined
+    if (this.length === 0) return undefined
     let temp = this.head
     this.head = this.head.next
-    temp.next = null
     this.length--
-    if (this.length == 0) this.tail = null
+    if (this.length === 0) {
+      this.tail = null
+    }
+    temp.next = null
     return temp
   }
 
   get(index) {
-    if (index < 1 || index >= this.length) return undefined
+    if (index < 0 || index >= this.length) return undefined
     let temp = this.head
     for (let i = 0; i < index; i++) {
       temp = temp.next
@@ -86,8 +120,36 @@ class LinkedList {
   }
 }
 
-let myLinkedList = new LinkedList(7)
-myLinkedList.push(4)
-console.log("result", myLinkedList.shift())
-console.log("result", myLinkedList.shift())
-console.log("result", myLinkedList.shift())
+function test() {
+  let myLinkedList = new LinkedList(0)
+  myLinkedList.push(1)
+  myLinkedList.push(2)
+  myLinkedList.push(3)
+
+  console.log("Linked List before set():")
+  myLinkedList.printList()
+
+  myLinkedList.set(2, 99)
+
+  console.log("\nLinked List after set():")
+  myLinkedList.printList()
+}
+
+test()
+
+/*
+  EXPECTED OUTPUT:
+  ----------------
+  Linked List before set():
+  0
+  1
+  2
+  3
+  
+  Linked List after set():
+  0
+  1
+  99
+  3
+
+*/
